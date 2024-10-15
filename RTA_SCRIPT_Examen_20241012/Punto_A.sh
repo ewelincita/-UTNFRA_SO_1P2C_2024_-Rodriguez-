3212d1d0883ec4716036a2fa1d6eac58e1419ae7
ewelincita <evelinmilirodriguez@gmail.com>
2024-10-15 21:28:18 +0000

    ADD: realice Punto A

---
+++ RTA_SCRIPT_Examen_20241012/Punto_A.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+#Resolución del punto A, cree un directorio para tener su estructrura
+
+
+
+sudo mkdir -p /Examenes-UTN/{alumno1/parcial{1,2,3},alumno2/parcial{1,2,3},alumno3/parcial{1,2,3},profesores}
